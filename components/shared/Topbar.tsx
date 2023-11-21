f237f1350f852d27488ae699b6ab7980d17706a1
Joseph <gadiazajoseph18@gmail.com>
2023-11-21 17:35:38 +0800

    setting up the home page layout

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <nav className="topbar">
+        <Link href="/" className="flex items-center gap-4">
+            <Image src="/logo.svg" alt="logo" width={28} height={28} />
+            <p className="text-heading3-bold text-light-1 max-xs:hidden">
+                Eagle Eye Connect
+            </p>
+        </Link>
+    </nav>
+  )
+}
+
+export default Topbar
