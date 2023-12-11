437c16da72f080a8109ac2e5e7a7168218803b99
Joseph <gadiazajoseph18@gmail.com>
2023-12-11 13:17:08 +0800

    have prepared for getting the webhooks secret in clerk

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,41 +1,46 @@
-import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Topbar = () => {
   return (
     <nav className="topbar">
-        <Link href="/" className="flex items-center gap-4">
-            <Image src="/assets/logo.svg" alt="logo" width={28} height={28} className="rounded-md bg-white" />
-            <p className="text-heading3-bold text-light-1 max-xs:hidden">
-                Eagle Eye Connect
-            </p>
-        </Link>
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          Eagle Eye Connect
+        </p>
+      </Link>
 
-        <div className="flex items-center gap-1">
-          <div className="block sm:hidden">
-            <SignedIn>
-              <SignOutButton>
-                <div className="flex cursor-pointer">
-                  <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
-                </div>
-              </SignOutButton>
-            </SignedIn>
-          </div>
-
-          <OrganizationSwitcher 
-            appearance={{
-              baseTheme: dark,
-              elements: {
-                organizationSwitcherTrigger: "py-2 px-4"
-              }
-            }} 
-          />
+      <div className="flex items-center gap-1">
+        <div className="block sm:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
         </div>
-    </nav>
-  )
-}
 
-export default Topbar
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Topbar;
